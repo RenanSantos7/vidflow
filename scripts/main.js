7c784554f85456189c99eb0a20c6fcdcdcbadb2d
RenanSantos7 <renansantos7@proton.me>
2023-12-22 09:31:23 -0300

    Aula 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,35 @@
 const containerVideos = document.querySelector('.videos__container')
 
-const api = fetch('http://localhost:3000/videos')
-	.then(response => response.json())
-	.then(videos => {
-		videos.forEach(video => {
-			containerVideos.innerHTML += `
-                    <div class='videos__item'>
-                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                        <div class='descricao-video'>
-                            <img class='img-canal' src='${video.imagem || './assets/sidebar/Avatar_Alura.png'}' aria-hidden='true' />
-                            <h3 class='titulo-video'>${video.titulo}</h3>
-                            <p class='titulo-canal'>${video.descricao}</p>
-                        </div>
-                    </div>
-                `
-		})
-    })
-    .catch(error => {
-        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`
-    })
+async function getVideos() {
+    try {
+        const busca = await fetch('http://localhost:3000/videos')
+        const videos = await busca.json()
+        videos.forEach(video => {
+            if (video.categoria == '') {
+                throw new Error(`Video "${video.titulo}" sem categoria`)
+            }
+ 
+            containerVideos.innerHTML += `
+            <div class='videos__item'>
+                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class='descricao-video'>
+                    <img class='img-canal' src='${video.imagem || './assets/sidebar/Avatar_Alura.png'}' aria-hidden='true' />
+                    <h3 class='titulo-video'>${video.titulo}</h3>
+                    <p class='titulo-canal'>${video.descricao}</p>
+                </div>
+            </div>
+        `
+        })
+    }
+    catch (error) {
+        containerVideos.innerHTML = `
+            <div class='erro'>
+                <img src='/assets/emoji-triste.svg' alt='carinha triste' class='erro__emoji' />
+                <p class='erro__texto'>Houve um erro ao carregar os vídeos...</p>
+                <p class='erro__texto error'>${error}</p>
+            </div>
+        `
+    }
+}
 
-
+getVideos()
